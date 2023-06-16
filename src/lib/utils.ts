2aa7c42974dfdfd956c609a03ee80fec89b38daf
Tobias Foerg <tobias.foerg@gmail.com>
2023-06-16 09:41:59 +0200

    feat!: allow hex values

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
 import { SetOptional } from "type-fest";
-import { Colors, ColorsToCamleCase, Theme, ThemeConfig, ToHslString } from "./types";
+import { ColorsToCamleCase, HslString, HslTuple, Theme, ThemeColors, ThemeConfig, ToHslString } from "./types";
 
 export function defineTheme<T extends SetOptional<ThemeConfig, "dark">>(config: T): Theme {
 	const { base, dark, light } = config;
@@ -12,10 +12,50 @@ export function defineTheme<T extends SetOptional<ThemeConfig, "dark">>(config: 
 	};
 }
 
-function toCSSRuleObject(colors: ColorsToCamleCase<Colors>): ToHslString<Colors> {
-	return Object.entries(colors).reduce<ToHslString<Colors>>((agg, [key, value]) => {
-		agg[("--" + key.replace(/[A-Z]/g, (letter) => `-${letter.toLowerCase()}`)) as keyof ToHslString<Colors>] =
-			typeof value === "string" ? value : `${value[0]} ${value[1]}% ${value[2]}%`;
+function toCSSRuleObject(colors: ColorsToCamleCase<ThemeColors>): ToHslString<ThemeColors> {
+	return Object.entries(colors).reduce<ToHslString<ThemeColors>>((agg, [key, value]) => {
+		agg[("--" + key.replace(/[A-Z]/g, (letter) => `-${letter.toLowerCase()}`)) as keyof ToHslString<ThemeColors>] =
+			typeof value === "string"
+				? /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(value)
+					? hslToSting(rgbToHsl(value))
+					: (value as HslString)
+				: `${value[0]} ${value[1]}% ${value[2]}%`;
 		return agg;
 	}, {} as any);
+}
+
+function hslToSting(value: HslTuple): HslString {
+	return `${value[0]} ${value[1]}% ${value[2]}%`;
+}
+
+function rgbToHsl(hex: string): HslTuple {
+	const r = parseInt(hex.substring(1, 3), 16) / 255;
+	const g = parseInt(hex.substring(3, 5), 16) / 255;
+	const b = parseInt(hex.substring(5, 7), 16) / 255;
+	const max = Math.max(r, g, b);
+	const min = Math.min(r, g, b);
+	let h = 0;
+	let s = 0;
+	let l = (max + min) / 2;
+
+	if (max === min) {
+		h = s = 0; // achromatic
+	} else {
+		var d = max - min;
+		s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+		switch (max) {
+			case r:
+				h = (g - b) / d + (g < b ? 6 : 0);
+				break;
+			case g:
+				h = (b - r) / d + 2;
+				break;
+			case b:
+				h = (r - g) / d + 4;
+				break;
+		}
+		h /= 6;
+	}
+
+	return [h, s * 360, l * 360];
 }
